36bb62c8bd16c1eb7a7194a0141a9c0eb9731334
Syuzi <106294093+SyuziGh@users.noreply.github.com>
2022-06-03 16:41:32 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends React.Component{
             </ul>
           </div>
           <Routes>
-            <Route exact path="/" element={<HomePage/>}/>
+            <Route exact path="https://syuzigh.github.io/shopdemo/" element={<HomePage/>}/>
             <Route  path="man" element={<ManPage/>}>
               <Route path="shirts" element={<ManShirts/>}/>
               <Route path="shoes" element={<ManShoes/>}/>
